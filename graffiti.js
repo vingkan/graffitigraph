09e6426d8a1edb893bcbe9e955138b16978a5f1b
Vinesh Kannan <vingkan@gmail.com>
2015-10-16 19:47:55 -0500

    Refining graffiti data model

---
+++ graffiti.js
@@ -0,0 +1,26 @@
+var graffitiIDList = [];
+
+function newGraffitiID(){
+	var newID = "graffiti" + graffitiIDList.length;
+	graffitiIDList.push(newID);
+	return newID;
+}
+
+
+Graffiti.prototype.coordinates = {
+	'latitude': 0.0,
+	'longitude': 0.0
+}; //Object of Doubles/Floats
+
+function Graffiti(data){
+	this.id = newGraffitiID();
+	/*this.species = data['tree_species'] || this.species;
+	this.name = data['common_name'] || this.name;
+	this.diameter = parseInt(data['diameter_at_breast_height'], 10) || this.diameter;
+	this.trunks = parseInt(data['number_of_trunks'], 10) || this.trunks;
+	this.location = data['location_type'] || this.location;*/
+	this.coordinates = {
+		'latitude': parseFloat(data['location']['latitude']),
+		'longitude': parseFloat(data['location']['longitude'])
+	} || this.coordinates;
+}
