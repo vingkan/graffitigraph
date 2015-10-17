5aa4a9680996b103dbd2b0b059dd1b9e0dee420f
Vinesh Kannan <vingkan@gmail.com>
2015-10-16 20:22:01 -0500

    Center on first data point and loop through all markers in query

--- map.js
+++ map.js
@@ -11,11 +11,25 @@ function updateCoords(position){
 navigator.geolocation.getCurrentPosition(updateCoords);
 
 function initGoogleMap(markerArray){
+	var centerPoint = markerArray[0];
 	var mapProperties = {
-		center: new google.maps.LatLng(userLocation.latitude, userLocation.longitude),
+		//center: new google.maps.LatLng(userLocation.latitude, userLocation.longitude),
+		center: new google.maps.LatLng(centerPoint.getLat(), centerPoint.getLon()),
 		zoom: 18,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var mapDiv = document.getElementById('googleMap');
 	var googleMap = new google.maps.Map(mapDiv, mapProperties);
+
+	for(var m = 0; m < markerArray.length; m++){
+		var current = markerArray[m];
+		var marker = new google.maps.Marker({
+			position: {
+				lat: current.getLat(),
+				lng: current.getLon()
+			},
+			map: googleMap,
+			title: current.name
+		});
+	}
 }
