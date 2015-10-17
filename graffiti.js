3c00f5911b83ccf7767b56ed9bef14b1f36ee6cf
Vinesh Kannan <vingkan@gmail.com>
2015-10-17 01:26:46 -0500

    Completed data model

--- graffiti.js
+++ graffiti.js
@@ -2,7 +2,10 @@ Graffiti.prototype.id = ""; //String
 Graffiti.prototype.type = ""; //String
 Graffiti.prototype.requested = ""; //Date
 Graffiti.prototype.resolved = ""; //Date
+Graffiti.prototype.intersection = false; //Boolean
 Graffiti.prototype.address = ""; //String
+Graffiti.prototype.crossStreet = ""; //String
+Graffiti.prototype.surface = ""; //String, perhaps Enum eventually
 Graffiti.prototype.monikers = []; //String[]
 Graffiti.prototype.coordinates = {
 	'latitude': 0.0,
@@ -14,7 +17,14 @@ function Graffiti(data){
 	this.type = data['graffititype'];
 	this.requested = new Date(data['request_date']);
 	this.resolved = new Date(data['resolved_date']);
+	var intersectionType = false;
+	if(data['addrtype'] == 'I'){
+		intersectionType = true;
+	}
+	this.intersection = intersectionType;
 	this.address = data['address'];
+	this.crossStreet = data['crossstreet'];
+	this.surface = null;
 	this.monikers = [
 			data['graffitimoniker'],
 			data['graffitimoniker2'],
