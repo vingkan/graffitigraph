068f1ff71fc82a5f6d0fa81f8e34daae11f41f6a
Vinesh Kannan <vingkan@gmail.com>
2015-10-19 11:12:07 -0500

    Added pretty.js and collapseEntryDiv()

---
+++ demo/pretty.js
@@ -0,0 +1,8 @@
+function collapseEntryText(){
+	var entryDiv = document.getElementById('entry');
+	var googleMap = document.getElementById('googleMap');
+
+	entryDiv.style.width = "100vw";
+	entryDiv.style.height = "10vh";
+	googleMap.style.height = "90vh";
+}
